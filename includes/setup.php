5254b8dad72ffb5a3b47999086e9705e347b49eb
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-04-29 10:08:43 +0100

    fix: home page styles

--- includes/setup.php
+++ includes/setup.php
@@ -3,7 +3,7 @@
     if ( ! function_exists( 'mos_support' ) ) :
         function mos_support() {
             add_theme_support( 'wp-block-styles' );
-            add_editor_style( get_template_directory_uri() . '/assets/styles/editor.css' );
+            add_editor_style( get_template_directory_uri() . '/assets/styles/front.css' );
 
             // Register nav menus.
             register_nav_menus( array(
